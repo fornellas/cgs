b073730c3510db6261ca475c3c6e67bb3a73262d
Fabio Pugliese Ornellas <fabio.ornellas@gmail.com>
2025-11-09 18:40:27 +0000

    add real time command

--- grbl/grbl.go
+++ grbl/grbl.go
@@ -9,6 +9,55 @@ import (
 	"go.bug.st/serial"
 )
 
+type RealTimeCommand byte
+
+var (
+	// Soft-Reset
+	RealTimeCommandSoftReset RealTimeCommand = 0x18
+	// Status Report Query
+	RealTimeCommandStatusReportQuery RealTimeCommand = '?'
+	// Cycle Start / Resume
+	RealTimeCommandCycleStartResume RealTimeCommand = '~'
+	// Feed Hold
+	RealTimeCommandFeedHold RealTimeCommand = '!'
+	// Safety Door
+	RealTimeCommandSafetyDoor RealTimeCommand = 0x84
+	// Jog Cancel
+	RealTimeCommandJogCancel RealTimeCommand = 0x85
+	// Feed Override: Set 100% of programmed rate.
+	RealTimeCommandFeedOverrideSet100OfProgrammedRate RealTimeCommand = 0x90
+	// Feed Override: Increase 10%
+	RealTimeCommandFeedOverrideIncrease10 RealTimeCommand = 0x91
+	// Feed Override: Decrease 10%
+	RealTimeCommandFeedOverrideDecrease10 RealTimeCommand = 0x92
+	// Feed Override: Increase 1%
+	RealTimeCommandFeedOverrideIncrease1 RealTimeCommand = 0x93
+	// Feed Override: Decrease 1%
+	RealTimeCommandFeedOverrideDecrease1 RealTimeCommand = 0x94
+	// Rapid Override: Set to 100% full rapid rate.
+	RealTimeCommandRapidOverrideSetTo100FullRapidRate RealTimeCommand = 0x95
+	// Rapid Override: Set to 50% of rapid rate.
+	RealTimeCommandRapidOverrideSetTo50OfRapidRate RealTimeCommand = 0x96
+	// Rapid Override: Set to 25% of rapid rate.
+	RealTimeCommandRapidOverrideSetTo25OfRapidRate RealTimeCommand = 0x97
+	// Spindle Speed Override: Set 100% of programmed spindle speed
+	RealTimeCommandSpindleSpeedOverrideSet100OfProgrammedSpindleSpeed RealTimeCommand = 0x99
+	// Spindle Speed Override: Increase 10%
+	RealTimeCommandSpindleSpeedOverrideIncrease10 RealTimeCommand = 0x9A
+	// Spindle Speed Override: Decrease 10%
+	RealTimeCommandSpindleSpeedOverrideDecrease10 RealTimeCommand = 0x9B
+	// Spindle Speed Override: Increase 1%
+	RealTimeCommandSpindleSpeedOverrideIncrease1 RealTimeCommand = 0x9C
+	// Spindle Speed Override: Decrease 1%
+	RealTimeCommandSpindleSpeedOverrideDecrease1 RealTimeCommand = 0x9D
+	// Toggle Spindle Stop
+	RealTimeCommandToggleSpindleStop RealTimeCommand = 0x9E
+	// Toggle Flood Coolant
+	RealTimeCommandToggleFloodCoolant RealTimeCommand = 0xA0
+	// Toggle Mist Coolant
+	RealTimeCommandToggleMistCoolant RealTimeCommand = 0xA1
+)
+
 type Grbl struct {
 	portName string
 	port     serial.Port
@@ -64,7 +113,27 @@ func (g *Grbl) Close() (err error) {
 	return
 }
 
-func (g *Grbl) Send(block string) error {
+// SendRealTimeCommand issues a real time command to Grbl.
+func (g *Grbl) SendRealTimeCommand(cmd RealTimeCommand) error {
+	data := []byte{byte(cmd)}
+	n, err := g.port.Write(data)
+	if err != nil {
+		return fmt.Errorf("grbl: write to serial port error: %s: %w", g.portName, err)
+	}
+	if n != len(data) {
+		return fmt.Errorf("grbl: write to serial port error: %s: wrote %d bytes, expected %d", g.portName, n, len(data))
+	}
+	if err := g.port.Drain(); err != nil {
+		return fmt.Errorf("grbl: serial port drain error: %s: %w", g.portName, err)
+	}
+	return nil
+}
+
+// TODO Jogging
+
+// TODO Synchronization
+
+func (g *Grbl) SendBlock(block string) error {
 	line := append([]byte(block), '\n')
 	n, err := g.port.Write(line)
 	if err != nil {
