7d3144170c4c75d9a8783f05413d58d74555708a
Fabio Pugliese Ornellas <fabio.ornellas@gmail.com>
2025-11-18 11:50:20 +0000

    extract command dispatcher

---
+++ control/command_dispatcher.go
@@ -0,0 +1,179 @@
+package control
+
+import (
+	"bytes"
+	"context"
+	"errors"
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/gdamore/tcell/v2"
+	"github.com/rivo/tview"
+
+	"github.com/fornellas/cgs/gcode"
+	grblMod "github.com/fornellas/cgs/grbl"
+)
+
+type CommandDispatcher struct {
+	grbl                       *grblMod.Grbl
+	appManager                 *AppManager
+	quietGcodeParserStateComms bool
+	quietGcodeParamStateComms  bool
+	quietStatusComms           bool
+}
+
+func NewCommandDispatcher(
+	grbl *grblMod.Grbl,
+	appManager *AppManager,
+	quietGcodeParserStateComms bool,
+	quietGcodeParamStateComms bool,
+	quietStatusComms bool,
+) *CommandDispatcher {
+	return &CommandDispatcher{
+		grbl:                       grbl,
+		appManager:                 appManager,
+		quietGcodeParserStateComms: quietGcodeParserStateComms,
+		quietGcodeParamStateComms:  quietGcodeParamStateComms,
+		quietStatusComms:           quietStatusComms,
+	}
+}
+
+//gocyclo:ignore
+func (cd *CommandDispatcher) SendCommand(
+	ctx context.Context,
+	command string,
+) {
+	// Extract and send real-time commands
+	var buf bytes.Buffer
+	for _, c := range []byte(command) {
+		rtc, err := grblMod.NewRealTimeCommand(c)
+		if err != nil {
+			if !errors.Is(err, grblMod.ErrNotRealTimeCommand) {
+				fmt.Fprintf(cd.appManager.CommandsTextView, "[%s]Real time command parsing fail: %s[-]\n", tcell.ColorRed, tview.Escape(err.Error()))
+				return
+			}
+			buf.WriteByte(c)
+		} else {
+			cd.sendRealTimeCommand(rtc)
+		}
+	}
+	command = buf.String()
+
+	if len(command) == 0 {
+		return
+	}
+
+	// Verbosity & timeout
+	var quiet bool
+	timeout := 1 * time.Second
+	parser := gcode.NewParser(strings.NewReader(command))
+	for {
+		block, err := parser.Next()
+		if err != nil {
+			fmt.Fprintf(cd.appManager.CommandsTextView, "[%s]Failed to parse: %s[-]\n", tcell.ColorRed, tview.Escape(err.Error()))
+			return
+		}
+		if block == nil {
+			break
+		}
+		if block.IsSystem() {
+			switch block.String() {
+			case "$G":
+				if cd.quietGcodeParserStateComms {
+					quiet = true
+				}
+			case "$#":
+				if cd.quietGcodeParamStateComms {
+					quiet = true
+				}
+			case "$H":
+				timeout = 120 * time.Second
+			}
+		}
+	}
+
+	// send command
+	if !quiet {
+		fmt.Fprintf(cd.appManager.CommandsTextView, "[%s]%s[-]\n", tcell.ColorWhite, tview.Escape(command))
+	}
+	ctx, cancel := context.WithDeadline(ctx, time.Now().Add(timeout))
+	defer cancel()
+	messageResponse, err := cd.grbl.SendCommand(ctx, command)
+	if err != nil {
+		fmt.Fprintf(cd.appManager.CommandsTextView, "[%s]Send command failed: %s[-]\n", tcell.ColorRed, tview.Escape(err.Error()))
+		return
+	}
+	if quiet {
+		return
+	}
+	if messageResponse.Error() == nil {
+		fmt.Fprintf(cd.appManager.CommandsTextView, "[%s]%s[-]\n", tcell.ColorGreen, tview.Escape(messageResponse.String()))
+	} else {
+		fmt.Fprintf(cd.appManager.CommandsTextView, "[%s]%s[-]\n", tcell.ColorRed, tview.Escape(messageResponse.String()))
+		fmt.Fprintf(cd.appManager.CommandsTextView, "[%s]%s[-]\n", tcell.ColorRed, tview.Escape(messageResponse.Error().Error()))
+	}
+}
+
+func (cd *CommandDispatcher) RunSendCommandWorker(ctx context.Context, sendCommandCh chan string) error {
+	for {
+		select {
+		case <-ctx.Done():
+			err := ctx.Err()
+			if errors.Is(err, context.Canceled) {
+				err = nil
+			}
+			return err
+		case command := <-sendCommandCh:
+			cd.appManager.CommandInputField.SetDisabled(true)
+			cd.appManager.HomingButton.SetDisabled(true)
+			cd.appManager.UnlockButton.SetDisabled(true)
+			// s.shellApp.joggingButton.SetDisabled(true)
+			cd.appManager.CheckButton.SetDisabled(true)
+			cd.appManager.SleepButton.SetDisabled(true)
+			// s.shellApp.settingsButton.SetDisabled(true)
+			cd.SendCommand(ctx, command)
+			// Sending $G enables tracking of G-Code parsing state
+			cd.SendCommand(ctx, "$G")
+			// Sending $G enables tracking of G-Code parameters
+			cd.SendCommand(ctx, "$#")
+			cd.appManager.CommandInputField.SetText("")
+			cd.appManager.CommandInputField.SetDisabled(false)
+			cd.appManager.HomingButton.SetDisabled(false)
+			cd.appManager.UnlockButton.SetDisabled(false)
+			// s.shellApp.joggingButton.SetDisabled(false)
+			cd.appManager.CheckButton.SetDisabled(false)
+			cd.appManager.SleepButton.SetDisabled(false)
+			// s.shellApp.settingsButton.SetDisabled(false)
+		}
+	}
+}
+
+func (cd *CommandDispatcher) sendRealTimeCommand(
+	cmd grblMod.RealTimeCommand,
+) {
+	if cd.quietStatusComms || cmd != grblMod.RealTimeCommandStatusReportQuery {
+		fmt.Fprintf(cd.appManager.CommandsTextView, "[%s]%s[-]\n", tcell.ColorWhite, tview.Escape(cmd.String()))
+	}
+	if err := cd.grbl.SendRealTimeCommand(cmd); err != nil {
+		fmt.Fprintf(cd.appManager.CommandsTextView, "[%s]Failed to send soft reset: %s[-]\n", tcell.ColorRed, err)
+	}
+}
+
+func (cd *CommandDispatcher) RunSendRealTimeCommandWorker(
+	ctx context.Context,
+	sendRealTimeCommandCh chan grblMod.RealTimeCommand,
+) error {
+	for {
+		select {
+		case <-ctx.Done():
+			err := ctx.Err()
+			if errors.Is(err, context.Canceled) {
+				err = nil
+			}
+			return err
+		case realTimeCommand := <-sendRealTimeCommandCh:
+			cd.sendRealTimeCommand(realTimeCommand)
+		}
+	}
+}
--- control/shell.go
+++ control/shell.go
@@ -1,19 +1,14 @@
 package control
 
 import (
-	"bytes"
 	"context"
 	"errors"
 	"fmt"
 	"log/slog"
-	"strings"
 	"time"
 
 	"github.com/fornellas/slogxt/log"
-	"github.com/gdamore/tcell/v2"
-	"github.com/rivo/tview"
 
-	"github.com/fornellas/cgs/gcode"
 	grblMod "github.com/fornellas/cgs/grbl"
 )
 
@@ -39,149 +34,7 @@ func NewControl(grbl *grblMod.Grbl, options *ControlOptions) *Control {
 	}
 }
 
-//gocyclo:ignore
-func (s *Control) sendCommand(
-	ctx context.Context,
-	command string,
-) {
-	// Extract and send real-time commands
-	var buf bytes.Buffer
-	for _, c := range []byte(command) {
-		rtc, err := grblMod.NewRealTimeCommand(c)
-		if err != nil {
-			if !errors.Is(err, grblMod.ErrNotRealTimeCommand) {
-				fmt.Fprintf(s.AppManager.CommandsTextView, "[%s]Real time command parsing fail: %s[-]\n", tcell.ColorRed, tview.Escape(err.Error()))
-				return
-			}
-			buf.WriteByte(c)
-		} else {
-			s.sendRealTimeCommand(rtc)
-		}
-	}
-	command = buf.String()
-
-	if len(command) == 0 {
-		return
-	}
-
-	// Verbosity & timeout
-	var quiet bool
-	timeout := 1 * time.Second
-	parser := gcode.NewParser(strings.NewReader(command))
-	for {
-		block, err := parser.Next()
-		if err != nil {
-			fmt.Fprintf(s.AppManager.CommandsTextView, "[%s]Failed to parse: %s[-]\n", tcell.ColorRed, tview.Escape(err.Error()))
-			return
-		}
-		if block == nil {
-			break
-		}
-		if block.IsSystem() {
-			switch block.String() {
-			case "$G":
-				if !s.options.DisplayGcodeParserStateComms {
-					quiet = true
-				}
-			case "$#":
-				if !s.options.DisplayGcodeParamStateComms {
-					quiet = true
-				}
-			case "$H":
-				timeout = 120 * time.Second
-			}
-		}
-	}
-
-	// send command
-	if !quiet {
-		fmt.Fprintf(s.AppManager.CommandsTextView, "[%s]%s[-]\n", tcell.ColorWhite, tview.Escape(command))
-	}
-	ctx, cancel := context.WithDeadline(ctx, time.Now().Add(timeout))
-	defer cancel()
-	messageResponse, err := s.grbl.SendCommand(ctx, command)
-	if err != nil {
-		fmt.Fprintf(s.AppManager.CommandsTextView, "[%s]Send command failed: %s[-]\n", tcell.ColorRed, tview.Escape(err.Error()))
-		return
-	}
-	if quiet {
-		return
-	}
-	if messageResponse.Error() == nil {
-		fmt.Fprintf(s.AppManager.CommandsTextView, "[%s]%s[-]\n", tcell.ColorGreen, tview.Escape(messageResponse.String()))
-	} else {
-		fmt.Fprintf(s.AppManager.CommandsTextView, "[%s]%s[-]\n", tcell.ColorRed, tview.Escape(messageResponse.String()))
-		fmt.Fprintf(s.AppManager.CommandsTextView, "[%s]%s[-]\n", tcell.ColorRed, tview.Escape(messageResponse.Error().Error()))
-	}
-}
-
-func (s *Control) sendCommandWorker(
-	ctx context.Context,
-	sendCommandCh chan string,
-) error {
-	for {
-		select {
-		case <-ctx.Done():
-			err := ctx.Err()
-			if errors.Is(err, context.Canceled) {
-				err = nil
-			}
-			return err
-		case command := <-sendCommandCh:
-			s.AppManager.CommandInputField.SetDisabled(true)
-			s.AppManager.HomingButton.SetDisabled(true)
-			s.AppManager.UnlockButton.SetDisabled(true)
-			// s.shellApp.joggingButton.SetDisabled(true)
-			s.AppManager.CheckButton.SetDisabled(true)
-			s.AppManager.SleepButton.SetDisabled(true)
-			// s.shellApp.settingsButton.SetDisabled(true)
-			s.sendCommand(ctx, command)
-			// Sending $G enables tracking of G-Code parsing state
-			s.sendCommand(ctx, "$G")
-			// Sending $G enables tracking of G-Code parameters
-			s.sendCommand(ctx, "$#")
-			s.AppManager.CommandInputField.SetText("")
-			s.AppManager.CommandInputField.SetDisabled(false)
-			s.AppManager.HomingButton.SetDisabled(false)
-			s.AppManager.UnlockButton.SetDisabled(false)
-			// s.shellApp.joggingButton.SetDisabled(false)
-			s.AppManager.CheckButton.SetDisabled(false)
-			s.AppManager.SleepButton.SetDisabled(false)
-			// s.shellApp.settingsButton.SetDisabled(false)
-		}
-	}
-}
-
-func (s *Control) sendRealTimeCommand(
-	cmd grblMod.RealTimeCommand,
-) {
-	if s.options.DisplayStatusComms || cmd != grblMod.RealTimeCommandStatusReportQuery {
-		fmt.Fprintf(s.AppManager.CommandsTextView, "[%s]%s[-]\n", tcell.ColorWhite, tview.Escape(cmd.String()))
-	}
-	if err := s.grbl.SendRealTimeCommand(cmd); err != nil {
-		fmt.Fprintf(s.AppManager.CommandsTextView, "[%s]Failed to send soft reset: %s[-]\n", tcell.ColorRed, err)
-	}
-}
-
-func (s *Control) sendRealTimeCommandWorker(
-	ctx context.Context,
-	sendRealTimeCommandCh chan grblMod.RealTimeCommand,
-) error {
-	for {
-		select {
-		case <-ctx.Done():
-			err := ctx.Err()
-			if errors.Is(err, context.Canceled) {
-				err = nil
-			}
-			return err
-		case realTimeCommand := <-sendRealTimeCommandCh:
-			s.sendRealTimeCommand(realTimeCommand)
-		}
-	}
-}
-
-func (s *Control) statusQueryWorker(ctx context.Context) error {
+func (c *Control) statusQueryWorker(ctx context.Context) error {
 	for {
 		select {
 		case <-ctx.Done():
@@ -191,18 +44,18 @@ func (s *Control) statusQueryWorker(ctx context.Context) error {
 			}
 			return err
 		case <-time.After(200 * time.Millisecond):
-			if err := s.grbl.SendRealTimeCommand(grblMod.RealTimeCommandStatusReportQuery); err != nil {
+			if err := c.grbl.SendRealTimeCommand(grblMod.RealTimeCommandStatusReportQuery); err != nil {
 				return fmt.Errorf("failed to send periodic status query real-time command: %w", err)
 			}
 		}
 	}
 }
 
-func (s *Control) Run(ctx context.Context) (err error) {
+func (c *Control) Run(ctx context.Context) (err error) {
 	logger := log.MustLogger(ctx)
 	logger.Info("Connecting")
 
-	pushMessageCh, err := s.grbl.Connect(ctx)
+	pushMessageCh, err := c.grbl.Connect(ctx)
 	if err != nil {
 		return err
 	}
@@ -219,49 +72,56 @@ func (s *Control) Run(ctx context.Context) (err error) {
 
 	statusQueryErrCh := make(chan error, 1)
 
-	s.AppManager = NewAppManager(sendCommandCh, sendRealTimeCommandCh)
-	defer func() { s.AppManager = nil }()
+	c.AppManager = NewAppManager(sendCommandCh, sendRealTimeCommandCh)
+	defer func() { c.AppManager = nil }()
 
 	logger = slog.New(NewViewLogHandler(
 		logger.Handler(),
-		s.AppManager.PushMessagesLogsTextView,
+		c.AppManager.PushMessagesLogsTextView,
 	))
 	ctx = log.WithLogger(ctx, logger)
 
+	commandDispatcher := NewCommandDispatcher(
+		c.grbl,
+		c.AppManager,
+		!c.options.DisplayGcodeParserStateComms,
+		!c.options.DisplayGcodeParamStateComms,
+		!c.options.DisplayStatusComms,
+	)
 	go func() {
 		defer cancelFn()
-		defer s.AppManager.App.Stop()
-		sendCommandWorkerErrCh <- s.sendCommandWorker(ctx, sendCommandCh)
+		defer c.AppManager.App.Stop()
+		sendCommandWorkerErrCh <- commandDispatcher.RunSendCommandWorker(ctx, sendCommandCh)
 	}()
 	go func() {
 		defer cancelFn()
-		defer s.AppManager.App.Stop()
-		sendRealTimeCommandWorkerErrCh <- s.sendRealTimeCommandWorker(
+		defer c.AppManager.App.Stop()
+		sendRealTimeCommandWorkerErrCh <- commandDispatcher.RunSendRealTimeCommandWorker(
 			ctx, sendRealTimeCommandCh,
 		)
 	}()
 	go func() {
 		defer cancelFn()
-		defer s.AppManager.App.Stop()
+		defer c.AppManager.App.Stop()
 		// Sending $G enables tracking of G-Code parsing state
-		s.sendCommand(ctx, "$G")
+		commandDispatcher.SendCommand(ctx, "$G")
 		// Sending $G enables tracking of G-Code parameters
-		s.sendCommand(ctx, "$#")
+		commandDispatcher.SendCommand(ctx, "$#")
 		messageProcessor := NewMessageProcessor(
-			s.grbl,
-			s.AppManager,
+			c.grbl,
+			c.AppManager,
 			pushMessageCh,
-			s.sendCommand,
-			!s.options.DisplayGcodeParserStateComms,
-			!s.options.DisplayGcodeParamStateComms,
-			!s.options.DisplayStatusComms,
+			commandDispatcher.SendCommand,
+			!c.options.DisplayGcodeParserStateComms,
+			!c.options.DisplayGcodeParamStateComms,
+			!c.options.DisplayStatusComms,
 		)
 		pushMessageErrCh <- messageProcessor.Run(ctx)
 	}()
 	go func() {
 		defer cancelFn()
-		defer s.AppManager.App.Stop()
-		statusQueryErrCh <- s.statusQueryWorker(ctx)
+		defer c.AppManager.App.Stop()
+		statusQueryErrCh <- c.statusQueryWorker(ctx)
 	}()
 
 	defer func() {
@@ -270,7 +130,7 @@ func (s *Control) Run(ctx context.Context) (err error) {
 		err = errors.Join(err, <-sendRealTimeCommandWorkerErrCh)
 		err = errors.Join(err, <-pushMessageErrCh)
 		err = errors.Join(err, <-statusQueryErrCh)
-		err = errors.Join(err, s.grbl.Disconnect())
+		err = errors.Join(err, c.grbl.Disconnect())
 	}()
-	return s.AppManager.App.Run()
+	return c.AppManager.App.Run()
 }
