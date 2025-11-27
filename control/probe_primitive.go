8909230553371db4c061415bd0ed08c06c7fc932
Fabio Pugliese Ornellas <fabio.ornellas@gmail.com>
2025-11-27 22:30:47 +0000

    more draft

--- control/probe_primitive.go
+++ control/probe_primitive.go
@@ -12,6 +12,14 @@ type ProbePrimitive struct {
 	*tview.Flex
 	app              *tview.Application
 	controlPrimitive *ControlPrimitive
+
+	straightMoveOrientationDropdown *tview.DropDown
+	straightXInputField             *tview.InputField
+	straightYInputField             *tview.InputField
+	straightZInputField             *tview.InputField
+	straightFeedRateInputField      *tview.InputField
+	straightProbeButton             *tview.Button
+	straightFlex                    *tview.Flex
 }
 
 func NewProbePrimitive(
@@ -19,70 +27,120 @@ func NewProbePrimitive(
 	app *tview.Application,
 	controlPrimitive *ControlPrimitive,
 ) *ProbePrimitive {
-	sp := &ProbePrimitive{
+	pp := &ProbePrimitive{
 		app:              app,
 		controlPrimitive: controlPrimitive,
 	}
 
-	// Move Orientation
-	moveOrientationDropdown := tview.NewDropDown()
-	moveOrientationDropdown.SetLabel("Move Orientation")
-	moveOrientationDropdown.SetOptions([]string{"Toward piece", "From piece"}, func(text string, index int) {
-		// TODO
-	})
-	moveOrientationDropdown.SetCurrentOption(0)
+	pp.newStraight()
 
-	xInputField := tview.NewInputField()
-	xInputField.SetFieldWidth(coordinateWidth)
-	xInputField.SetLabel("X")
-	xInputField.SetAcceptanceFunc(acceptFloat)
-	xInputField.SetChangedFunc(func(string) {
-		// TODO
-	})
-
-	yInputField := tview.NewInputField()
-	yInputField.SetLabel("Y")
-	yInputField.SetFieldWidth(coordinateWidth)
-	yInputField.SetAcceptanceFunc(acceptFloat)
-	yInputField.SetChangedFunc(func(string) {
-		// TODO
-	})
-
-	zInputField := tview.NewInputField()
-	zInputField.SetLabel("Z")
-	zInputField.SetFieldWidth(coordinateWidth)
-	zInputField.SetAcceptanceFunc(acceptFloat)
-	zInputField.SetChangedFunc(func(string) {
-		// TODO
-	})
-
-	feedRateInputField := tview.NewInputField()
-	feedRateInputField.SetLabel("Feed rate")
-	feedRateInputField.SetFieldWidth(feedWidth)
-	feedRateInputField.SetAcceptanceFunc(acceptUFloat)
-	feedRateInputField.SetChangedFunc(func(string) {
-		// TODO
-	})
-
-	probeButton := tview.NewButton("Probe")
-	probeButton.SetSelectedFunc(func() {
-		// TODO
-	})
+	// Angle
+	anfleFlex := tview.NewFlex()
+	anfleFlex.SetBorder(true)
+	anfleFlex.SetTitle("Angle")
+	anfleFlex.SetDirection(tview.FlexRow)
+	// TODO axis probe positions (first, second)
+	// TODO probe axis & direction (x-,x+,y-,y+)
+	// TODO feed rate
+	// TODO result: error | angle
 
 	rootFlex := tview.NewFlex()
 	rootFlex.SetBorder(true)
 	rootFlex.SetTitle("Probe")
 	rootFlex.SetDirection(tview.FlexRow)
-	rootFlex.AddItem(moveOrientationDropdown, 1, 0, false)
-	rootFlex.AddItem(xInputField, 1, 0, false)
-	rootFlex.AddItem(yInputField, 1, 0, false)
-	rootFlex.AddItem(zInputField, 1, 0, false)
-	rootFlex.AddItem(feedRateInputField, 1, 0, false)
-	rootFlex.AddItem(probeButton, 1, 0, false)
-	sp.Flex = rootFlex
+	rootFlex.AddItem(pp.straightFlex, 0, 1, false)
+	rootFlex.AddItem(anfleFlex, 0, 1, false)
+	pp.Flex = rootFlex
 
-	return sp
+	return pp
+}
+func (pp *ProbePrimitive) newStraight() {
+	// Move Orientation
+	straightMoveOrientationDropdown := tview.NewDropDown()
+	straightMoveOrientationDropdown.SetLabel("Move Orientation")
+	straightMoveOrientationDropdown.SetOptions([]string{"Toward piece [darkblue]G38.2[-]", "From piece [darkblue]G38.4[-]"}, func(text string, index int) {
+		// TODO
+	})
+	straightMoveOrientationDropdown.SetCurrentOption(0)
+	pp.straightMoveOrientationDropdown = straightMoveOrientationDropdown
+
+	// x
+	straightXInputField := tview.NewInputField()
+	straightXInputField.SetFieldWidth(coordinateWidth)
+	straightXInputField.SetLabel("X")
+	straightXInputField.SetAcceptanceFunc(acceptFloat)
+	straightXInputField.SetChangedFunc(func(string) {
+		// TODO
+	})
+	pp.straightXInputField = straightXInputField
+
+	// y
+	straightYInputField := tview.NewInputField()
+	straightYInputField.SetLabel("Y")
+	straightYInputField.SetFieldWidth(coordinateWidth)
+	straightYInputField.SetAcceptanceFunc(acceptFloat)
+	straightYInputField.SetChangedFunc(func(string) {
+		// TODO
+	})
+	pp.straightYInputField = straightYInputField
+
+	// z
+	straightZInputField := tview.NewInputField()
+	straightZInputField.SetLabel("Z")
+	straightZInputField.SetFieldWidth(coordinateWidth)
+	straightZInputField.SetAcceptanceFunc(acceptFloat)
+	straightZInputField.SetChangedFunc(func(string) {
+		// TODO
+	})
+	pp.straightZInputField = straightZInputField
+
+	// Feed rate
+	straightFeedRateInputField := tview.NewInputField()
+	straightFeedRateInputField.SetLabel("Feed rate")
+	straightFeedRateInputField.SetFieldWidth(feedWidth)
+	straightFeedRateInputField.SetAcceptanceFunc(acceptUFloat)
+	straightFeedRateInputField.SetChangedFunc(func(string) {
+		// TODO
+	})
+	pp.straightFeedRateInputField = straightFeedRateInputField
+
+	// Probe
+	straightProbeButton := tview.NewButton("Probe")
+	straightProbeButton.SetSelectedFunc(func() {
+		// TODO
+	})
+	pp.straightProbeButton = straightProbeButton
+
+	// Probe
+	probeFlex := tview.NewFlex()
+	probeFlex.SetDirection(tview.FlexRow)
+	probeFlex.AddItem(straightMoveOrientationDropdown, 1, 0, false)
+	probeFlex.AddItem(straightXInputField, 1, 0, false)
+	probeFlex.AddItem(straightYInputField, 1, 0, false)
+	probeFlex.AddItem(straightZInputField, 1, 0, false)
+	probeFlex.AddItem(straightFeedRateInputField, 1, 0, false)
+	probeFlex.AddItem(straightProbeButton, 1, 0, false)
+	// TODO probe result
+
+	// Set
+	setFlex := tview.NewFlex()
+	setFlex.SetBorder(true)
+	setFlex.SetTitle("Set")
+	// TODO add buttons to G92 (Coordinate System Offset) - X, Y, Z
+	// TODO add buttons to G92.1 (Reset G92 Offsets)
+	// TODO add buttons to G10 L2 P[1-9] (Set Coordinate System, By Offsets) - P, X, Y, Z
+	// TODO add buttons to G10 L20 P[1-9] (Set Coordinate System, From Current Position)
+
+	straightFlex := tview.NewFlex()
+	straightFlex.SetBorder(true)
+	straightFlex.SetTitle("Straight")
+	straightFlex.SetDirection(tview.FlexColumn)
+	straightFlex.AddItem(probeFlex, 0, 1, false)
+	straightFlex.AddItem(setFlex, 0, 1, false)
+	pp.straightFlex = straightFlex
 }
 
-func (sp *ProbePrimitive) ProcessPushMessage(ctx context.Context, pushMessage grblMod.PushMessage) {
+func (pp *ProbePrimitive) ProcessPushMessage(ctx context.Context, pushMessage grblMod.PushMessage) {
+	// TODO set last probing cycle status from g-code param message
+	// TODO reset last probing cycle status on welcome
 }
