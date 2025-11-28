ffdc84a9de7c34efdf2423389a347aed5c702bf7
Fabio Pugliese Ornellas <fabio.ornellas@gmail.com>
2025-11-28 02:43:31 +0000

    draft probe button

--- control/probe_primitive.go
+++ control/probe_primitive.go
@@ -1,12 +1,17 @@
 package control
 
 import (
+	"bytes"
 	"context"
+	"fmt"
 
 	"github.com/rivo/tview"
 
 	grblMod "github.com/fornellas/cgs/grbl"
 )
+
+var probeTowardPieceText = "Toward piece[lightblue]G38.2[-]"
+var probeFromPieceText = "From piece[lightblue]G38.4[-]"
 
 type ProbePrimitive struct {
 	*tview.Flex
@@ -58,7 +63,7 @@ func (pp *ProbePrimitive) newStraight() {
 	// Move Orientation
 	straightMoveOrientationDropdown := tview.NewDropDown()
 	straightMoveOrientationDropdown.SetLabel("Move Orientation ")
-	straightMoveOrientationDropdown.SetOptions([]string{"Toward piece[lightblue]G38.2[-]", "From piece[lightblue]G38.4[-]"}, func(text string, index int) {
+	straightMoveOrientationDropdown.SetOptions([]string{probeTowardPieceText, probeFromPieceText}, func(text string, index int) {
 		// TODO
 	})
 	straightMoveOrientationDropdown.SetCurrentOption(0)
@@ -107,37 +112,51 @@ func (pp *ProbePrimitive) newStraight() {
 	// Probe
 	straightProbeButton := tview.NewButton("Probe")
 	straightProbeButton.SetSelectedFunc(func() {
-		// TODO
+		n, moveOrientation := straightMoveOrientationDropdown.GetCurrentOption()
+		if n < 0 {
+			return
+		}
+		var buf bytes.Buffer
+		switch moveOrientation {
+		case probeTowardPieceText:
+			fmt.Fprintf(&buf, "G38.2")
+		case probeFromPieceText:
+			fmt.Fprintf(&buf, "G38.4")
+		default:
+			panic(fmt.Sprintf("bug: unknown move orientation option: %#v", moveOrientation))
+		}
+		if x := straightXInputField.GetText(); x != "" {
+			fmt.Fprintf(&buf, "X%s", x)
+		}
+		if y := straightYInputField.GetText(); y != "" {
+			fmt.Fprintf(&buf, "Y%s", y)
+		}
+		if z := straightZInputField.GetText(); z != "" {
+			fmt.Fprintf(&buf, "Z%s", z)
+		}
+
+		feedRate := straightFeedRateInputField.GetText()
+		if feedRate == "" {
+			return
+		}
+		fmt.Fprintf(&buf, "F%s", feedRate)
+
+		pp.controlPrimitive.QueueCommand(buf.String())
 	})
 	pp.straightProbeButton = straightProbeButton
 
-	// Probe
-	probeFlex := tview.NewFlex()
-	probeFlex.SetDirection(tview.FlexRow)
-	probeFlex.SetBorderPadding(1, 1, 1, 1)
-	probeFlex.AddItem(straightMoveOrientationDropdown, 1, 0, false)
-	probeFlex.AddItem(straightXInputField, 1, 0, false)
-	probeFlex.AddItem(straightYInputField, 1, 0, false)
-	probeFlex.AddItem(straightZInputField, 1, 0, false)
-	probeFlex.AddItem(straightFeedRateInputField, 1, 0, false)
-	probeFlex.AddItem(straightProbeButton, 3, 0, false)
-	// TODO probe result
-
-	// Set
-	setFlex := tview.NewFlex()
-	setFlex.SetBorder(true)
-	setFlex.SetTitle("Set")
-	// TODO add buttons to G92 (Coordinate System Offset) - X, Y, Z
-	// TODO add buttons to G92.1 (Reset G92 Offsets)
-	// TODO add buttons to G10 L2 P[1-9] (Set Coordinate System, By Offsets) - P, X, Y, Z
-	// TODO add buttons to G10 L20 P[1-9] (Set Coordinate System, From Current Position)
-
+	// Straight
 	straightFlex := tview.NewFlex()
 	straightFlex.SetBorder(true)
 	straightFlex.SetTitle("Straight")
-	straightFlex.SetDirection(tview.FlexColumn)
-	straightFlex.AddItem(probeFlex, 0, 1, false)
-	straightFlex.AddItem(setFlex, 0, 1, false)
+	straightFlex.SetDirection(tview.FlexRow)
+	straightFlex.AddItem(straightMoveOrientationDropdown, 1, 0, false)
+	straightFlex.AddItem(straightXInputField, 1, 0, false)
+	straightFlex.AddItem(straightYInputField, 1, 0, false)
+	straightFlex.AddItem(straightZInputField, 1, 0, false)
+	straightFlex.AddItem(straightFeedRateInputField, 1, 0, false)
+	straightFlex.AddItem(straightProbeButton, 3, 0, false)
+	// TODO probe result
 	pp.straightFlex = straightFlex
 }
 
